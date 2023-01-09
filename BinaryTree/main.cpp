0c6d9c2aff5ff242a714e7d33a68f3a0b74cbff1
mitchcamza <192mitch88@gmail.com>
2023-01-09 16:48:48 +0200

    Initial Commit

---
+++ BinaryTree/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  BinaryTree
+//
+//  Created by Mitch Campbell on 2023/01/09.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
